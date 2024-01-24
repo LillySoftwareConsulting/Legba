e6f4101e31c894126762015f227eee4c42565727
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2024-01-24 13:36:51 -0600

    Added About page

---
+++ Legba.WPF/Windows/About.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Legba.Engine.ViewModels;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Legba.WPF.Windows;
+
+public partial class About : Window
+{
+    public About(AboutViewModel aboutViewModel)
+    {
+        InitializeComponent();
+
+        DataContext = aboutViewModel;
+    }
+
+    private void OK_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
