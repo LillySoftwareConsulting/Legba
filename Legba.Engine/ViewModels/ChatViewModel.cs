dec166794ab8b09f32684daab6e03e6dadfd6574
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2024-01-25 10:07:00 -0600

    Checkpoint

--- Legba.Engine/ViewModels/ChatViewModel.cs
+++ Legba.Engine/ViewModels/ChatViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Windows.Input;
 using Legba.Engine.Models;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,16 +21,30 @@ public class ChatViewModel : ObservableObject
         get { return _chatSession; }
         set
         {
+            if (_chatSession != null)
+            {
+                _chatSession.Messages.CollectionChanged -= Messages_CollectionChanged;
+            }
+
             if (_chatSession != value)
             {
                 _chatSession = value;
                 OnPropertyChanged(nameof(ChatSession));
                 OnPropertyChanged(nameof(HasChatSession));
+                OnPropertyChanged(nameof(HasChatMessages));
+
+                _chatSession.Messages.CollectionChanged += Messages_CollectionChanged;
             }
         }
     }
 
+    private void Messages_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        OnPropertyChanged(nameof(HasChatMessages));
+    }
+
     public bool HasChatSession => ChatSession != null;
+    public bool HasChatMessages => ChatSession?.Messages.Count > 0;
 
     public ICommand SelectModelCommand { get; private set; }
     public ICommand AskCommand { get; private set; }
